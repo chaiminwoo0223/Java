6dc1f18ec90d99147bc30370983d98c302387a52
최민우 <chaiminwoo0223@naver.com>
2024-07-01 20:25:59 +0900

    Delete FirstClass directory

--- FirstClass/src/list1.java
+++
@@ -1,9 +0,0 @@
-public class list1 {
-    public static void main(String[] args) {
-        String[] beer = {"Kloud", "Cass", "terra", "Guinness"}; // 인덱스 번호 : 0 , 1 , 2 , 3
-        System.out.println(beer[0]); // Kloud
-        System.out.println(beer[1]); // Cass
-        System.out.println(beer[2]); // terra
-        System.out.println(beer[3]); // Guinness
-    }
-}
--- FirstClass/src/number1.java
+++
@@ -1,9 +0,0 @@
-public class number1 {
-    public static void main(String[] args) {
-        int num = 5;
-        if(num > 3){
-            num++;
-        }
-        System.out.print("num 의 값은 "+num);
-    }
-}
