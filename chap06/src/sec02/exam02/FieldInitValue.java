fba2543e818c8a70bb79e04b7cc8b4534fe333e9
최민우 <chaiminwoo0223@naver.com>
2024-07-16 21:53:39 +0900

    필드

---
+++ chap06/src/sec02/exam02/FieldInitValue.java
@@ -0,0 +1,22 @@
+package sec02.exam02;
+
+// 필드 자동 초기화
+public class FieldInitValue {
+	// 정수 타입
+	byte byteField;
+	char charField;
+	short shortField;
+	int intField;
+	long longField;
+
+	// 실수 타입
+	float floatField;
+	double doubleField;
+
+	// 논리 타입
+	boolean booleanField;
+
+	// 참조 타입
+	int[] arrField;
+	String referenceField;
+}
