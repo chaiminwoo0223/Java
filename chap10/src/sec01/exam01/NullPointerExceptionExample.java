03c69d2bb462b965648a83e53562105b2ee44154
최민우 <chaiminwoo0223@naver.com>
2024-07-30 23:37:38 +0900

    예외 클래스

---
+++ chap10/src/sec01/exam01/NullPointerExceptionExample.java
@@ -0,0 +1,9 @@
+package sec01.exam01;
+
+// NullPointerException
+public class NullPointerExceptionExample {
+	public static void main(String[] args) {
+		String data = null;
+		System.out.println(data.toString());
+	}
+}
---
+++ chap10/src/sec01/exam04/ClassCastExceptionExample.java
@@ -0,0 +1,20 @@
+package sec01.exam04;
+
+// ClassCastException
+public class ClassCastExceptionExample {
+	public static void main(String[] args) {
+		Dog dog = new Dog();
+		changeDog(dog);
+		Cat cat = new Cat();
+		changeDog(cat); // ClassCastException 발생
+	}
+
+	public static void changeDog(Animal animal) {
+		Dog dog = (Dog)animal;
+		System.out.println(dog);
+	}
+}
+
+class Animal {}
+class Dog extends Animal {}
+class Cat extends Animal {}
