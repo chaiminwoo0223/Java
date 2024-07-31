dc7dd3a776501025426be5f2bdaca49335904447
최민우 <chaiminwoo0223@naver.com>
2024-08-01 00:31:32 +0900

    java.lang 패키지

---
+++ chap11/src/sec01/exam05/SmartPhone.java
@@ -0,0 +1,17 @@
+package sec01.exam05;
+
+// 객체의 문자 정보(toString()메소드)
+public class SmartPhone {
+	private String company;
+	private String os;
+
+	public SmartPhone(String company, String os) {
+		this.company = company;
+		this.os = os;
+	}
+
+	@Override
+	public String toString() {
+		return company + ", " + os;
+	}
+}
