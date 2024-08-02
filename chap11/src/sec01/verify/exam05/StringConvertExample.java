4a2b980be416f6b662fdec519bf13a3f5904652c
최민우 <chaiminwoo0223@naver.com>
2024-08-02 21:35:39 +0900

    java.lang 패키지

--- chap11/src/sec01/verify/exam05/StringConvertExample.java
+++ chap11/src/sec01/verify/exam05/StringConvertExample.java
@@ -2,10 +2,12 @@ package sec01.verify.exam05;
 
 public class StringConvertExample {
 	public static void main(String[] args) {
+		// 문자열을 정수로 변환
 		String strData1 = "200";
 		int intData1 = Integer.parseInt(strData1);
 		System.out.println(intData1);
 
+		// 정수를 문자열로 변환
 		int intData2 = 150;
 		String strData2 = String.valueOf(intData2);
 		System.out.println(strData2);
