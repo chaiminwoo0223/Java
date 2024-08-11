c8aad21ca54838aa04bcc06cd52666efed99ef66
최민우 <chaiminwoo0223@naver.com>
2024-08-11 18:53:25 +0900

    Delete chap09/src/sec01 directory

--- chap09/src/sec01/exam06/CallListener.java
+++
@@ -1,9 +0,0 @@
-package sec01.exam06;
-
-// 구현 클래스
-public class CallListener implements Button.OnClickListener {
-	@Override
-	public void onClick() {
-		System.out.println("전화를 겁니다.");
-	}
-}
--- chap09/src/sec01/exam06/MessageListener.java
+++
@@ -1,9 +0,0 @@
-package sec01.exam06;
-
-// 구현 클래스
-public class MessageListener implements Button.OnClickListener {
-	@Override
-	public void onClick() {
-		System.out.println("메시지를 보냅니다.");
-	}
-}
