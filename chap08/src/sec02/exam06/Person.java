03d15d3b5b5af9d1105a8237439499862062445c
최민우 <chaiminwoo0223@naver.com>
2024-08-11 18:43:28 +0900

    인터페이스

---
+++ chap08/src/sec02/exam06/Person.java
@@ -0,0 +1,17 @@
+package sec02.exam06;
+
+// instanceof의 자동 타입 변환
+public class Person {
+	// 필드 선언
+	public String name;
+
+	// 생성자 선언
+	public Person(String name) {
+		this.name = name;
+	}
+
+	// 메소드 선언
+	public void walk() {
+		System.out.println("걷습니다.");
+	}
+}
