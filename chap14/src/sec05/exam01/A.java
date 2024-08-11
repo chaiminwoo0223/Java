1cd07e42a9ea464d0d14dbf5b0dacbf7214461d3
최민우 <chaiminwoo0223@naver.com>
2024-08-11 18:27:02 +0900

    중첩 클래스의 정적 멤버 허용

---
+++ chap14/src/sec05/exam01/A.java
@@ -0,0 +1,41 @@
+package sec05.exam01;
+
+// 중첩 클래스의 정적 멤버 허용
+public class A {
+	A() { System.out.println("A 객체가 생성됨"); }
+
+	// 인스턴스 멤버 클래스
+	class B {
+		B() { System.out.println("B 객체가 생성됨"); }
+		int field1;
+		static int field2;
+		void method1() {}
+		static void method2() {}
+	}
+
+	// 정적 멤버 클래스
+	static class C {
+		C() { System.out.println("C 객체가 생성됨"); }
+		int field1; // 인스턴스 필드
+		static int field2; // 정적 필드
+		void method1() {} // 인스턴스 메소드
+		static void method2() {} // 정적 메소드
+	}
+
+	void method() {
+		// 로컬 클래스
+		class D {
+			D() { System.out.println("D 객체가 생성됨"); }
+			int field1;
+			static int field2;
+			void method1() {}
+			static void method2() {}
+		}
+
+		D d = new D();
+		d.field1 = 3;
+		d.method1();
+		D.field2 = 3;
+		D.method2();
+	}
+}
---
+++ chap14/src/sec05/exam02/InstanceofExample.java
@@ -0,0 +1,24 @@
+package sec05.exam02;
+
+// instanceof의 자동 타입 변환
+public class InstanceofExample {
+	public static void main(String[] args) {
+		Person p1 = new Person("홍길동");
+		personInfo(p1);
+		System.out.println();
+
+		Person p2 = new Student("김길동", 10);
+		personInfo(p2);
+	}
+
+	public static void personInfo(Person person) {
+		System.out.println("name: " + person.name);
+		person.walk();
+
+		// person이 참조하는 객체가 Student 타입일 경우 자동 타입 변환
+		if(person instanceof Student student) {
+			System.out.println("studentNo: " + student.studentNo);
+			student.study();
+		}
+	}
+}
