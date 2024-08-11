f61c93b5f3c21b4b22b26aa5997cd758b70dcc49
최민우 <chaiminwoo0223@naver.com>
2024-08-11 16:07:41 +0900

    텍스트 블록

--- chap14/src/sec03/exam01/TextBlockExample1.java
+++ chap14/src/sec03/exam01/TextBlockExample1.java
@@ -1,9 +1,23 @@
 package sec03.exam01;
 
-// 텍스트 블록
+// 텍스트 블록 1
 public class TextBlockExample1 {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String str1 = "" +
+				"{\n" +
+				"\t\"id\":\"winter\",\n" +
+				"\t\"name\":\"눈송이\",\n" +
+				"}";
 
+		String str2 = """
+				{
+				        "id":"winter",
+				        "name":"눈송이"
+				}
+				""";
+
+		System.out.println(str1);
+		System.out.println("-------------------------");
+		System.out.println(str2);
 	}
 }
---
+++ chap14/src/sec03/exam02/TextBlockExample2.java
@@ -0,0 +1,15 @@
+package sec03.exam02;
+
+// 텍스트 블록 2
+public class TextBlockExample2 {
+	public static void main(String[] args) {
+		String str = """
+				나는 자바를 학습합니다.
+				나는 자바를 좋아합니다.
+
+				나는 자바를 사랑합니다.
+				""";
+
+		System.out.println(str);
+	}
+}
